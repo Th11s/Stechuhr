ef8f1047ef3835d27fb467b889b77fedd527600c
Aaron Puhl <aaron_puhl@web.de>
2023-05-08 08:55:36 +0200

    fundamental operations to track work times start, end of work and breaks

---
+++ src/TimeKeeping.Web/Client/AppStore.cs
@@ -0,0 +1,17 @@
+﻿using TimeKeeping.Web.Shared.HttpModel;
+
+namespace TimeKeeping.Web.Client
+{
+    public class AppStore
+    {
+        public Task Upsert(Entry entry)
+        {
+            return Task.Delay(11);
+        }
+
+        public Task RemoveEntry(Entry entry)
+        {
+            return Task.Delay(11);
+        }
+    }
+}
---
+++ src/TimeKeeping.Web/Shared/HttpModel/Entry.cs
@@ -0,0 +1,19 @@
+﻿namespace TimeKeeping.Web.Shared.HttpModel;
+
+public class Entry
+{
+    public Entry()
+    {
+        
+    }
+    public Entry(EntryType type, DateTimeOffset at, Guid? uuid = null)
+    {
+        Type = type;
+        At = at;
+        Uuid = uuid ?? Guid.NewGuid();
+    }
+
+    public EntryType Type { get; set; }
+    public DateTimeOffset At { get; set; }
+    public Guid Uuid { get; }
+}
