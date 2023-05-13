fe64bb3b20022fcdcdb821ab76ff6cf59508cbe6
aaron-puhl <aaron_puhl@web.de>
2023-05-13 08:24:51 +0200

    Time keeping fundamental operations frontend (#2)
    * design stub
    * fundamental operations to track work times
    start, end of work and breaks

---
+++ src/TimeKeeping.Web/Shared/HttpModel/EntryCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeKeeping.Web.Shared.HttpModel;
+
+public class EntryCollection : IValidatableObject
+{
+    public List<Entry> Entries { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+
+        var pauseCount = 0;
+        var workCount = 0;
+        foreach (var entry in Entries.OrderBy(x => x.At))
+        {
+            if (entry.Type == EntryType.WorkStart)
+            {
+                workCount++;
+            }
+
+            if (entry.Type == EntryType.WorkEnd)
+            {
+                workCount--;
+            }
+
+            if (entry.Type == EntryType.BreakStart)
+            {
+                pauseCount++;
+            }
+
+            if (entry.Type == EntryType.BreakEnd)
+            {
+                pauseCount--;
+            }
+
+            if (pauseCount < 0 || workCount < 0)
+            {
+                yield return new("invalid chronology");
+            }
+        }
+
+
+        if (pauseCount != 0 || workCount != 0)
+        {
+            yield return new("missing entries");
+        }
+    }
+}
