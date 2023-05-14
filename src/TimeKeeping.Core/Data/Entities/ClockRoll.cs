fd26e9c304a6618aeb68a30fd6b51ad6a3007d44
Ottenhus, Thomas <TG85+Github@outlook.de>
2023-05-14 21:06:40 +0200

    WIP

--- src/TimeKeeping.Core/Data/Entities/ClockRoll.cs
+++ src/TimeKeeping.Core/Data/Entities/ClockRoll.cs
@@ -12,8 +12,23 @@
         public AggregationInfo? Aggregation { get; set; }
 
 
+        /// <summary>
+        /// Enthält den gesamten Audit zum Eintrag.
+        /// Die Audits werden per JSON gespeichert, um später den Übergang in die Tages-, Wochen-, Monats- und Jahresaggregate zu vereinfachen.
+        /// </summary>
         public AuditCollection ChangeTracking { get; set; } = new();
+
+        /// <summary>
+        /// True, wenn der Eintrag verändert, oder retrospektiv gespeichert wurde.
+        /// </summary>
         public bool HasBeenModified { get; set; }
-        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// True, wenn der Eintrag durch einen Benutzer gelöscht wurde.
+        /// Wird nur benötigt, um den Audit korrekt aufrecht zu erhalten.
+        /// ChangeTracking enthält einen entsprechenden Eintrag.
+        /// Weitere Änderungen am Eintrag sind dadruch verhindert
+        /// </summary>
+        public bool HasBeenDeleted { get; set; }
     }
 }
