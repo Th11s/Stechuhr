08fd030e699301812195cf5d3a866ab5b5975fa7
Thomas Ottenhus <glatzert@uni-mainz.de>
2023-09-14 17:42:13 +0200

    Core service (#3)
    Datenmodellentwurf

---
+++ src/TimeKeeping.Core/Data/Entities/Abteilung.cs
@@ -0,0 +1,17 @@
+﻿namespace Th11s.TimeKeeping.Data.Entities
+{
+    public class Abteilung
+    {
+        public Abteilung(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        
+        public string DisplayName { get; set; }
+
+        public string? ExternalId { get; set; }
+    }
+}
