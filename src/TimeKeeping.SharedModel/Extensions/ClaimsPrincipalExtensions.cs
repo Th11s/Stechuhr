674837e55df60ad1a341f82c00b1e58d88d7eeb5
Ottenhus, Thomas <TG85+Github@outlook.de>
2023-10-08 15:41:32 +0200

    Hilfsmethoden für Claims

---
+++ src/TimeKeeping.SharedModel/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+using Th11s.TimeKeeping.SharedModel.Primitives;
+
+namespace Th11s.TimeKeeping.SharedModel.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetDisplayName(this ClaimsPrincipal p) 
+            => p.FindFirst("name")?.Value;
+
+        public static string[] GetArbeitsplatzIds(this ClaimsPrincipal p)
+            => p.FindAll(CustomClaimTypes.Arbeitsplatz).Select(x => x.Value).ToArray();
+    }
+}
