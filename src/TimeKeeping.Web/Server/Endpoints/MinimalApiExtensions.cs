4149729dfa876f5f3780cd40086a7620ac5bfec0
Ottenhus, Thomas <TG85+Github@outlook.de>
2023-10-08 13:28:07 +0200

    Add MinimalAPI implementations

--- src/TimeKeeping.Web/Server/Endpoints/MinimalApiExtensions.cs
+++ src/TimeKeeping.Web/Server/Endpoints/MinimalApiExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Th11s.TimeKeeping.SharedModel.Web;
+﻿using System.Net;
+using Th11s.TimeKeeping.SharedModel.Web;
 
 namespace TimeKeeping.Web.Server.Endpoints
 {
@@ -8,8 +9,17 @@ namespace TimeKeeping.Web.Server.Endpoints
         {
             var api = app.MapGroup("/api/v1");
             api.MapGet("/{arbeitsplatzId:guid}/{jahr:int}-{monat:int}-{tag:int}", Zeiterfassung.Tagesansicht)
-                .Produces(200, typeof(Tagessicht))
-                .Produces(400);
+                .Produces((int)HttpStatusCode.OK, typeof(Tagessicht))
+                .Produces((int)HttpStatusCode.BadRequest);
+
+            api.MapGet("/{arbeitsplatzId:guid}/{jahr:int}-{monat:int}", Zeiterfassung.Monatsansicht)
+                .Produces((int)HttpStatusCode.OK, typeof(Monatssicht))
+                .Produces((int)HttpStatusCode.BadRequest);
+
+            api.MapPost("/{arbeitsplatzId:guid}/", Zeiterfassung.ErfasseZeitstempel)
+                .Produces((int)HttpStatusCode.OK)
+                .Produces((int)HttpStatusCode.Created)
+                .Produces((int)HttpStatusCode.BadRequest);
         }
     }
 }
