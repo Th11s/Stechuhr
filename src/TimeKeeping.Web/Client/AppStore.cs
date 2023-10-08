ec11f741c7219cea289b6112ac11f0507e3e7316
Aaron Puhl <aaron_puhl@web.de>
2023-10-08 16:08:30 +0200

    start store

--- src/TimeKeeping.Web/Client/AppStore.cs
+++ src/TimeKeeping.Web/Client/AppStore.cs
@@ -1,17 +1,116 @@
-﻿using TimeKeeping.Web.Shared.HttpModel;
+﻿using Th11s.TimeKeeping.SharedModel.Requests;
+using TimeKeeping.Web.Client.Clients;
+using TimeKeeping.Web.Shared.HttpModel;
 
 namespace TimeKeeping.Web.Client
 {
     public class AppStore
     {
-        public Task Upsert(Entry entry)
+        private Task _initTask;
+        private Queue<StechzeitCommand> _queue = new();
+        private readonly TimeKeepingClient _client;
+
+        public AppStore(TimeKeepingClient client)
         {
-            return Task.Delay(11);
+            _client = client;
+            _initTask = InitializeAsync();
         }
 
-        public Task RemoveEntry(Entry entry)
+        public Task InitializeAsync()
         {
-            return Task.Delay(11);
+            if (_initTask != null)
+                return _initTask;
+
+            _queue = new();//TODO load from storage
+            _ = RunSync();
+
+            return Task.CompletedTask;
         }
+
+        private async Task RunSync()
+        {
+            if (_queue.Any())
+            {
+                try
+                {
+                    while (_queue.Any() )
+                    {
+                        var command = _queue.Peek();
+                        if (command.Action == StechzeitAction.Upsert)
+                        {
+                            var zeitstempelRequest = new ZeitstempelRequest
+                            {
+                                Datum = DateOnly.FromDateTime(command.Entry.At.ToLocalTime().Date),
+                                Stempeltyp = command.Entry.Type,
+                                Zeitstempel = command.Entry.At,
+                            };
+                            await _client.ErfasseZeitstempelAsync(command.ArbeitsplatzUuid, zeitstempelRequest);
+                        }
+                        else if (command.Action == StechzeitAction.Delete)
+                        {
+                            //TODO
+                        }
+
+                        //TODO persist queue
+                        _queue.Dequeue();
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+            _ = Task.Delay(1000 * 60 * 15).ContinueWith(_ => RunSync());
+        }
+
+        public async Task Upsert(Guid arbeitsplatzUuid, Entry entry)
+        {
+            await _initTask;
+
+            if (_queue.Any())
+            {
+                _queue.Enqueue(new StechzeitCommand { ArbeitsplatzUuid = arbeitsplatzUuid, Action = StechzeitAction.Upsert, Entry = entry });
+                //TODO persist queue
+            }
+            else
+            {
+                try
+                {
+                    var zeitstempelRequest = new ZeitstempelRequest
+                    {
+                        Datum = DateOnly.FromDateTime(entry.At.ToLocalTime().Date),
+                        Stempeltyp = entry.Type,
+                        Zeitstempel = entry.At,
+                    };
+                    await _client.ErfasseZeitstempelAsync(arbeitsplatzUuid, zeitstempelRequest);
+                }
+                catch
+                {
+                    _queue.Enqueue(new StechzeitCommand { ArbeitsplatzUuid = arbeitsplatzUuid, Action = StechzeitAction.Upsert, Entry = entry });
+                    //TODO persist queue
+                }
+            }
+        }
+
+        public async Task RemoveEntry(Entry entry)
+        {
+            await _initTask;
+            await Task.Delay(11);
+        }
+    }
+
+    //TODO: Erlauben wir andere offline Aktionen?
+    internal class StechzeitCommand
+    {
+        public required Guid ArbeitsplatzUuid { get; set; }
+        public required StechzeitAction Action { get; set; }
+        public required Entry Entry { get; set; }
+    }
+
+    internal enum StechzeitAction
+    {
+        Upsert,
+        Delete
     }
 }
