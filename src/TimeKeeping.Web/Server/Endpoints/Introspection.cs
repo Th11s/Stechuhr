f193477fcf9dce34f05389478d18f8b23cf34538
Ottenhus, Thomas <TG85+Github@outlook.de>
2023-10-08 15:42:00 +0200

    /_app/User

--- src/TimeKeeping.Web/Server/Endpoints/Introspection.cs
+++ src/TimeKeeping.Web/Server/Endpoints/Introspection.cs
@@ -1,6 +1,19 @@
-﻿namespace TimeKeeping.Web.Server.Endpoints
+﻿
+using Th11s.TimeKeeping.SharedModel.Web;
+
+namespace TimeKeeping.Web.Server.Endpoints
 {
     public static class Introspection
     {
+        internal static IResult User(HttpContext context, CancellationToken ct)
+        {
+            if (!context.User.Identities.Any(x => x.IsAuthenticated))
+                return Results.Unauthorized();
+
+            return new AppUser(
+                context.User.GetDisplayName(),
+                context.User.Claims.Select(x => (x.Type, x.Value)).ToList()
+                );
+        }
     }
 }
